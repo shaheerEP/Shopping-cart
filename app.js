10e39af931844f86699e981ef57fb072d5be732b
Shaheer ep <shaheerep121.com>
2024-05-28 15:34:55 -0700

    commit 5.2

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const helpers = require('./config/handlebars-helpers');
 const cloudinary = require('cloudinary').v2;
 const fileUpload = require('express-fileupload');
 
-cloudinary.config({
+cloudinary.config({ 
   cloud_name: process.env.CLOUD_NAME, 
   api_key: process.env.CLOUD_API_KEY , 
   api_secret: process.env.CLOUD_SECRET_KEY,
